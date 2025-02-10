9ac7086ac432261a549fa606d2db2795fec12020
Althea Gonzales <altheagonzalesctr@gmail.com>
2025-02-10 08:20:45 +0800

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,52 @@
+class Account {
+    private String accountNumber;
+    private double balance;
+    private AccountState accountState;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountState = new ActiveState(); // Default state is Active
+    }
+
+    public void deposit(double amount) {
+        accountState.deposit(this, amount);
+    }
+
+    public void withdraw(double amount) {
+        accountState.withdraw(this, amount);
+    }
+
+    public void activate() {
+        accountState.activate(this);
+    }
+
+    public void suspend() {
+        accountState.suspend(this);
+    }
+
+    public void close() {
+        accountState.close(this);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setState(AccountState state) {
+        this.accountState = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Account Number: " + accountNumber + ", Balance: $" + balance;
+    }
+}
